4b346d1acbbd9a678d267ec799d446e92aea4942
Alex Vasyutin <alex.vasyutin@gmail.com>
2026-05-27 19:19:08 +0200

    Enable kickstart/plugins/mini.lua for kvim

--- dotfiles/kvim/lua/kickstart/plugins/mini.lua
+++ dotfiles/kvim/lua/kickstart/plugins/mini.lua
@@ -39,6 +39,41 @@ return {
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function() return '%2l:%-2v' end
 
+      -- Start screen inspired by the classic Vim startify setup.
+      local starter = require 'mini.starter'
+      local starter_header = function()
+        if vim.fn.executable 'fortune' == 1 and vim.fn.executable 'cowsay' == 1 then
+          local lines = vim.fn.systemlist 'fortune | cowsay -f tux'
+          if vim.v.shell_error == 0 and #lines > 0 then return table.concat(lines, '\n') end
+        end
+
+        return table.concat({
+          ' _             _',
+          '| | ____   _(_)_ __ ___',
+          "| |/ /\\ \\ / / | '_ ` _ \\",
+          '|   <  \\ V /| | | | | | |',
+          '|_|\\_\\  \\_/ |_|_| |_| |_|',
+        }, '\n')
+      end
+
+      starter.setup {
+        header = starter_header,
+        footer = "Vim is charityware. Please read ':help Uganda'.",
+        items = {
+          starter.sections.recent_files(8, true, true),
+          starter.sections.recent_files(8, false, true),
+          { name = 'Find files', action = 'Telescope find_files', section = 'Telescope' },
+          { name = 'Live grep', action = 'Telescope live_grep', section = 'Telescope' },
+          { name = 'Recent files', action = 'Telescope oldfiles', section = 'Telescope' },
+          { name = 'Help tags', action = 'Telescope help_tags', section = 'Telescope' },
+          starter.sections.builtin_actions(),
+        },
+        content_hooks = {
+          starter.gen_hook.adding_bullet(),
+          starter.gen_hook.aligning('center', 'center'),
+        },
+      }
+
       -- ... and there is more!
       --  Check out: https://github.com/nvim-mini/mini.nvim
     end,
--- dotfiles/kvim/lua/kickstart/plugins/telescope.lua
+++ dotfiles/kvim/lua/kickstart/plugins/telescope.lua
@@ -61,11 +61,18 @@ return {
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
+      local actions = require 'telescope.actions'
+
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
         defaults = {
+          mappings = {
+            i = {
+              ['<Esc>'] = actions.close,
+            },
+          },
           sorting_strategy = 'ascending',
           layout_config = {
             prompt_position = 'top',
