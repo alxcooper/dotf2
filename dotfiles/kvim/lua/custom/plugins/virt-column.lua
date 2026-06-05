a6b2d21d547d931c7075d9b0520a2e0116f007e2
Alex Vasyutin <alex.vasyutin@gmail.com>
2026-06-05 19:41:25 +0200

    Add nvim nice virt-column

---
+++ dotfiles/kvim/lua/custom/plugins/virt-column.lua
@@ -0,0 +1,26 @@
+-- virt-column.nvim - draw a thin text-width guide instead of a colorcolumn band.
+return {
+  {
+    'lukas-reineke/virt-column.nvim',
+    lazy = false,
+    config = function(_, opts)
+      vim.api.nvim_set_hl(0, 'VirtColumnMuted', { fg = '#424254' })
+      local virt_column = require 'virt-column'
+
+      virt_column.setup(opts)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('custom-python-virt-column', { clear = true }),
+        pattern = 'python',
+        callback = function(event)
+          virt_column.setup_buffer(event.buf, { virtcolumn = '88' })
+        end,
+      })
+    end,
+    opts = {
+      char = '┊',
+      virtcolumn = '110',
+      highlight = 'VirtColumnMuted',
+    },
+  },
+}
