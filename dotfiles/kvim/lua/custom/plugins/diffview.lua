2565cc0232e5e372ff2caf5fd0eaeee2421dad40
Alex Vasyutin <alex.vasyutin@gmail.com>
2026-09-16 20:22:25 +0200

    nvim: Improve Diffview navigation and staging workflow

--- dotfiles/kvim/lua/custom/plugins/diffview.lua
+++ dotfiles/kvim/lua/custom/plugins/diffview.lua
@@ -4,6 +4,23 @@
 --   :DiffviewOpen origin/master...HEAD  -- everything on your branch vs origin/master
 --   :DiffviewOpen HEAD~3                -- last 3 commits
 --   :DiffviewFileHistory %              -- history of current file
+local function stage_and_focus()
+  local view = require('diffview.lib').get_current_view()
+  if not view then
+    return
+  end
+
+  -- Diffview already advances to the next entry after staging/unstaging.
+  require('diffview.actions').toggle_stage_entry()
+  if view.cur_layout then
+    view.cur_layout:get_main_win():focus()
+  end
+end
+
+local function focus_entry()
+  require('diffview.actions').focus_entry()
+end
+
 return {
   {
     'dlyongemallo/diffview.nvim',
@@ -24,13 +41,30 @@ return {
     },
     opts = {
       enhanced_diff_hl = true,
+      keymaps = {
+        view = {
+          { 'n', '-', stage_and_focus, { desc = 'Stage / unstage and focus the next file' } },
+        },
+        file_panel = {
+          { 'n', '-', stage_and_focus, { desc = 'Stage / unstage and focus the next file' } },
+          { 'n', '<cr>', focus_entry, { desc = 'Open file and focus its contents' } },
+        },
+        file_history_panel = {
+          { 'n', '<cr>', focus_entry, { desc = 'Open file and focus its contents' } },
+        },
+      },
       view = {
+        cycle_layouts = {
+          -- g<C-x>: side by side -> unified inline -> top/bottom.
+          default = { 'diff2_horizontal', 'diff1_inline', 'diff2_vertical' },
+        },
         merge_tool = {
           layout = 'diff3_mixed',
         },
       },
       hooks = {
         diff_buf_win_enter = function(_, winid, _)
+          vim.wo[winid].wrap = false
           -- Cursorline only on the line number in diff windows, to avoid the
           -- bg conflict that renders as an underline on diff-highlighted lines.
           -- Workaround from: https://github.com/sindrets/diffview.nvim/issues/113
